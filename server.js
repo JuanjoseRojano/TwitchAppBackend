3f3834461464e94b222653363f3ad8741811e69d
Juan Jose Rojano Doncel <jjanjor@gmail.com>
2026-05-20 13:36:19 +0200

    Prueba correción URI

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get("/login", (req, res) => {
     const authUrl =
         `https://id.twitch.tv/oauth2/authorize` +
         `?client_id=${client_id}` +
-        `redirect_uri=${encodeURIComponent(redirect_uri)}` +
+        `redirect_uri=${'&redirect_uri='+redirect_uri}` +
         `response_type=code` +
         `scope=${encodeURIComponent(scope)}`;
 
